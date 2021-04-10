d5ca7097d15c989b2fd3e390a7d1744dc4dc4c0b
Clement Boiteux <67805925+iTakedown27@users.noreply.github.com>
2021-04-09 19:35:58 -0700

    make sure stuff gets committed

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const express = require('express')
 const http = require('http')
 const app = express()
+const port = 8080
 
-app.get('/')
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.send('Up and running!')
+})
+
+app.listen(port, () => {
+
+})
